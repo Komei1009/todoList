ce01aa4427469cd351b70803fbdf4dd8102c896b
Komei1009 <1994komei1009@gmail.com>
2018-03-10 20:57:03 +0900

    api ver1.0

---
+++ controller/todo.go
@@ -0,0 +1,34 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+// Todo新規追加
+func TodoAdd(c *gin.Context){
+
+	
+}
+
+// Todo完了
+func TodoComplete(c *gin.Context){
+
+}
+
+// Todo削除
+func TodoRemove(c *gin.Context){
+
+}
+
+// 全てのTodo
+func AllTodo(c *gin.Context){
+
+}
+
+// 実行中のTodo
+func ActiveTodo(c *gin.Context){
+
+}
+
+// 完了したTodo
+func CompletedTodo(c *gin.Context){
+
+}
